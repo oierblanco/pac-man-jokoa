72bd85abd68467f5b771b4e6960dc8ed05743282
oierblanco <93262346+oierblanco@users.noreply.github.com>
2021-12-01 14:28:06 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 let pacman = game.createSprite(2, 4)
 let gost = game.createSprite(2, 2)
 let janaria = game.createSprite(randint(0, 4), randint(0, 4))
-gost.set(LedSpriteProperty.Blink, 0)
-gost.set(LedSpriteProperty.Blink, 0)
-janaria.set(LedSpriteProperty.Blink, 0)
+gost.set(LedSpriteProperty.Blink, 5)
+gost.set(LedSpriteProperty.Brightness, 100)
+janaria.set(LedSpriteProperty.Brightness, 100)
 basic.forever(function () {
     if (input.acceleration(Dimension.X) > 200) {
         pacman.move(1)
         basic.pause(200)
     }
-    if (input.acceleration(Dimension.X) < 200) {
+    if (input.acceleration(Dimension.X) < -200) {
         pacman.move(-1)
         basic.pause(200)
     }
@@ -17,7 +17,7 @@ basic.forever(function () {
         pacman.change(LedSpriteProperty.Y, 1)
         basic.pause(200)
     }
-    if (input.acceleration(Dimension.Y) < 200) {
+    if (input.acceleration(Dimension.Y) < -200) {
         pacman.change(LedSpriteProperty.Y, -1)
         basic.pause(200)
     }
