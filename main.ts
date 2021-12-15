309067d0239124d294082d5282beed24c3e6626e
oierblanco <93262346+oierblanco@users.noreply.github.com>
2021-12-15 14:26:34 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,24 @@ gost.set(LedSpriteProperty.Blink, 5)
 gost.set(LedSpriteProperty.Brightness, 100)
 janaria.set(LedSpriteProperty.Brightness, 100)
 basic.forever(function () {
+    if (pacman.isTouching(gost)) {
+        game.gameOver()
+    }
+})
+basic.forever(function () {
+    if (pacman.isTouching(janaria)) {
+        game.addScore(1)
+        janaria.delete()
+        janaria = game.createSprite(randint(0, 4), randint(0, 4))
+        janaria.set(LedSpriteProperty.Brightness, 100)
+    }
+})
+basic.forever(function () {
+    basic.pause(400)
+    gost.move(1)
+    gost.ifOnEdgeBounce()
+})
+basic.forever(function () {
     if (input.acceleration(Dimension.X) > 200) {
         pacman.move(1)
         basic.pause(200)
@@ -20,23 +38,5 @@ basic.forever(function () {
     if (input.acceleration(Dimension.Y) < -200) {
         pacman.change(LedSpriteProperty.Y, -1)
         basic.pause(200)
-    }
-})
-basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        basic.pause(400)
-        gost.move(1)
-    }
-    gost.ifOnEdgeBounce()
-})
-basic.forever(function () {
-    if (pacman.isTouching(gost)) {
-        game.gameOver()
-    }
-})
-basic.forever(function () {
-    if (pacman.isTouching(janaria)) {
-        game.addScore(1)
-        janaria.delete()
     }
 })
